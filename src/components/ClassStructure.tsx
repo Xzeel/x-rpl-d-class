dae4c1e1e66baed6a23acdc5175a3dd490627b8e
XzeelArcadia <138985421+Xzeel@users.noreply.github.com>
2025-12-07 09:28:50 +0700

    Add new groups and members to ClassStructure

--- src/components/ClassStructure.tsx
+++ src/components/ClassStructure.tsx
@@ -23,6 +23,30 @@ const ClassStructure = () => {
       title: 'Bendahara',
       members: ['1. Nesya Putri Anggraini', '2. Yesica Gianita Wijaya Putri'],
     },
+    {
+      title: 'Kebersihan',
+      members: ['1. Yesica', '2. Ajeng', '3. Serli', '4. Nana', '5. Rainer', '6. Bintang', '7. Shulthan'],
+    },
+    {
+      title: 'Keamanan',
+      members: ['1. Satria', '2. Fadhil', '3. Abi', '4. Akbar', '5. Ken'],
+    },
+    {
+      title: 'Ketertiban',
+      members: ['1. Fafa', '2. Irgy', '3. Naufal', '4. Esya', '5. Widi', '6. Gendis'],
+    },
+    {
+      title: 'Keindahan',
+      members: ['1. Safa', '2. Gendis Diva', '3. Caca', '4. Asa', '5. Nesya', '6. Nana'],
+    },
+    {
+      title: 'Kekeluargaan',
+      members: ['1. Dinda', '2. Rani', '3. Rahma', '4. Izza', '5. Cia'],
+    },
+    {
+      title: 'Keagamaan',
+      members: ['1. Abi', '2. Fadhil', '3. Raihan', '4. Fafa', '5. Moreno'],
+    },
   ];
 
   return (
